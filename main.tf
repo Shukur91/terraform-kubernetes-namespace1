f4729f87c9dd5953d935690731d25d3e47792dcf
Shukur91 <ssean6754@gmail.com>
2024-09-04 18:33:28 -0500

    added next version

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ resource "kubernetes_namespace" "this" {
     name = var.name
   }
 }
+
+resource "kubernetes_resource_quota" "pod" {
+  metadata {
+    name = "pod-quota"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+  spec {
+    hard = {
+      pods = var.number_of_pods
+    }
+    scopes = ["BestEffort"]
+  }
+}
+
+
+
--- variable.tf
+++ variable.tf
@@ -3,3 +3,9 @@ variable "name" {
   default     = ""
   description = "The name of the namespace"
 }
+
+variable number_of_pods {
+  type        = number
+  default     = "36"
+  description = "The number of pods to create"
+}
